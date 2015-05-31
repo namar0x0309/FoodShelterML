f095f5fbd303bf9835aa16d3c2c9a53749524449
biancaflaidar <flaidarb@seattleu.edu>
2015-05-31 11:38:33 -0700

    Routing

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var portfinder = require('portfinder');
 var ml = require('machine_learning');
-var jade = require('jade');
 
 // Code to include external javascript files.
 var fs = require('fs');
@@ -15,49 +14,55 @@ var includeInThisContext = function(path) {
 // Include javascript files.
 includeInThisContext(__dirname + "/machinelearning.js");
 
- // frontend  
-////var indexTemplatePath = 'views/index.jade';
-//var donorTemplatePath = 'views/donor.jade';
-//var shelterTemplatePath = 'views/shelter.jade';
-//var householdTemplatePath = 'views/household.jade';
+// frontend
+var jade = require('jade');
+var express = require('express');
 
-////var index = fs.readFileSync(indexTemplatePath, 'utf8');
-////var fn = jade.compile(index);
-////var tmp = fn();
-//var donorHTML = jade.compileFile(donorTemplatePath)(userObject);
-//var shelterHTML = jade.compileFile(shelterTemplatePath)(userObject);
-//var householdHTMl = jade.compileFile(householdTemplatePath)(userObject);
+var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
 portfinder.getPort(function (err, port) {
 	console.log( 'Port open:' + port);
-	
-	http.createServer(function (req, res) {
-  
-        // Response
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        
-        /*
-        res.end('Food Shelter Predictor\n' +
-                'Getting Database....\n' +
-                'Learning...\n');
-        */
-        res.write(tmp); // index template
-        res.end();
+	   
+    // // // learning
+    logisticRegressionTest();
                             
-        logisticRegressionTest();
+    console.log( '  \n \n \n ');
                             
-        console.log( '  \n \n \n ');
+    kMeansClusteringTest();
                             
-        kMeansClusteringTest();
+    console.log( '\n\n\n');
                             
-        console.log( '\n\n\n');
-                            
-        decisionTreeTest();
-	
-	
-	}).listen( port, '127.0.0.1' );
+    decisionTreeTest();
     
-	console.log('Server running at http://127.0.0.1:' + port + '/' );
+    // // //
+	        
+    app.get('/', function(req, res) {
+        res.render('index', {
+        title: 'Home' 
+        });
+    });
+    
+    app.get('/donor', function(req, res) {
+        res.render('donor', {
+            title: 'Donor' 
+       });
+    });
+    
+    app.get('/household', function(req, res) {
+        res.render('household', {
+            title: 'Household' 
+       });
+    });
+    
+    app.get('/shelter', function(req, res) {
+        res.render('shelter', {
+            title: 'Shelter'
+        });
+    });
+    
+    app.listen(8000);
  });
  
 
